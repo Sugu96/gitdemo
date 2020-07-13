7c3f1c5b9bdceeb336028cb763795568c4e5e2a5
sugu333 <sugavanam1996@gmail.com>
2020-07-13 14:48:34 +0530

    this is commit from asian guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,8 +7,13 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
+	
+	
+	
 	
 	@Test
 	public void deleteTwitter()
